522e1b1cde95eb988c39144acf775a9527b93445
Brandon Aubrey <baubrey91@gmail.com>
2025-03-14 22:24:30 -0700

    Initial Commit

---
+++ CAPI/CAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAPIApp.swift
+//  CAPI
+//
+//  Created by Brandon Aubrey on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CAPITests/CAPITests.swift
@@ -0,0 +1,17 @@
+//
+//  CAPITests.swift
+//  CAPITests
+//
+//  Created by Brandon Aubrey on 3/14/25.
+//
+
+import Testing
+@testable import CAPI
+
+struct CAPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
